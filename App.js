f9e089e40d9154fc5d6ac712a7f5f0a277e1a2af
BestProductsAlways <127046853+BestProductsAlways@users.noreply.github.com>
2023-08-14 05:56:52 -0230

    update 2

--- App.js
+++ App.js
@@ -1,20 +1,109 @@
-import ExpenseCategoryBox from './ExpenseCategoryBox';
-import React from 'react';
-import expenseCategories from './expenseCategories.json';
+import './styles.css';
 
-function App() {
-    return (
-        <div className="App">
-            {expenseCategories.map(category => (
-                <ExpenseCategoryBox
-                    key={category.label}
-                    label={category.label}
-                    maxBudget={category.maxBudget}
-                />
-            ))}
-        </div>
-    );
-}
+// App.js
+import React, { useState } from 'react';
+
+const categoriesData = [
+  'Groceries',
+  'Entertainment',
+  'Dining',
+  'Transportation',
+  // Add more categories as needed
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState([]);
+  const [category, setCategory] = useState('');
+  const [expenseItem, setExpenseItem] = useState('');
+  const [cost, setCost] = useState('');
+  const [purchaseDate, setPurchaseDate] = useState('');
+
+  const addExpense = () => {
+    if (!category || !expenseItem || !cost || !purchaseDate) {
+      alert('Please fill in all fields.');
+      return;
+    }
+
+    const newExpense = {
+      category,
+      expenseItem,
+      cost,
+      purchaseDate,
+      used: false,
+    };
+
+    setExpenses([...expenses, newExpense]);
+    setCategory('');
+    setExpenseItem('');
+    setCost('');
+    setPurchaseDate('');
+  };
+
+  const handleDelete = (index) => {
+    const updatedExpenses = [...expenses];
+    updatedExpenses.splice(index, 1);
+    setExpenses(updatedExpenses);
+  };
+
+  const handleCheckboxChange = (index) => {
+    const updatedExpenses = [...expenses];
+    updatedExpenses[index].used = !updatedExpenses[index].used;
+    setExpenses(updatedExpenses);
+  };
+
+  return (
+    <div className="container">
+      <h1>Expense Tracker</h1>
+      <div className="input-section">
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">Select Category</option>
+          {categoriesData.map((cat) => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Expense Item"
+          value={expenseItem}
+          onChange={(e) => setExpenseItem(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Cost"
+          value={cost}
+          onChange={(e) => setCost(e.target.value)}
+        />
+        <input
+          type="date"
+          placeholder="Purchase Date"
+          value={purchaseDate}
+          onChange={(e) => setPurchaseDate(e.target.value)}
+        />
+        <button onClick={addExpense}>Add</button>
+      </div>
+      <div className="expense-list">
+        {expenses.map((expense, index) => (
+          <div key={index} className="expense-item">
+            <span>{expense.category}</span>
+            <span>{expense.expenseItem}</span>
+            <span>{expense.cost}</span>
+            <span>{expense.purchaseDate}</span>
+            <input
+              type="checkbox"
+              checked={expense.used}
+              onChange={() => handleCheckboxChange(index)}
+            />
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
-
--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+// styles
+import "./App.css";
 
-// components
-import { MainContextProvider } from "./context/MainContext";
+import { Route, Routes } from "react-router-dom";
 
 // pages
 import ExpensesPage from "./pages/ExpensesPage";
-import NotFoundPage from "./pages/NotFoundPage";
 import InsightsPage from "./pages/InsightsPage";
-
-// styles
-import "./App.css";
+// components
+import { MainContextProvider } from "./context/MainContext";
+import NotFoundPage from "./pages/NotFoundPage";
+import React from "react";
 
 function App() {
   return (
@@ -23,7 +22,112 @@ function App() {
     //   </Routes>
     // </MainContextProvider>
     <InsightsPage />
+    
   );
 }
+
+const categoriesData = [
+  'Groceries',
+  'Entertainment',
+  'Dining',
+  'Transportation',
+  // Add more categories as needed
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState([]);
+  const [category, setCategory] = useState('');
+  const [expenseItem, setExpenseItem] = useState('');
+  const [cost, setCost] = useState('');
+  const [purchaseDate, setPurchaseDate] = useState('');
+
+  const addExpense = () => {
+    if (!category || !expenseItem || !cost || !purchaseDate) {
+      alert('Please fill in all fields.');
+      return;
+    }
+
+    const newExpense = {
+      category,
+      expenseItem,
+      cost,
+      purchaseDate,
+      used: false,
+    };
+
+    setExpenses([...expenses, newExpense]);
+    setCategory('');
+    setExpenseItem('');
+    setCost('');
+    setPurchaseDate('');
+  };
+
+  const handleDelete = (index) => {
+    const updatedExpenses = [...expenses];
+    updatedExpenses.splice(index, 1);
+    setExpenses(updatedExpenses);
+  };
+
+  const handleCheckboxChange = (index) => {
+    const updatedExpenses = [...expenses];
+    updatedExpenses[index].used = !updatedExpenses[index].used;
+    setExpenses(updatedExpenses);
+  };
+
+  return (
+    <div className="container">
+      <h1>Expense Tracker</h1>
+      <div className="input-section">
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">Select Category</option>
+          {categoriesData.map((cat) => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
+        </select>
+        <input
+          type="text"
+          placeholder="Expense Item"
+          value={expenseItem}
+          onChange={(e) => setExpenseItem(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Cost"
+          value={cost}
+          onChange={(e) => setCost(e.target.value)}
+        />
+        <input
+          type="date"
+          placeholder="Purchase Date"
+          value={purchaseDate}
+          onChange={(e) => setPurchaseDate(e.target.value)}
+        />
+        <button onClick={addExpense}>Add</button>
+      </div>
+      <div className="expense-list">
+        {expenses.map((expense, index) => (
+          <div key={index} className="expense-item">
+            <span>{expense.category}</span>
+            <span>{expense.expenseItem}</span>
+            <span>{expense.cost}</span>
+            <span>{expense.purchaseDate}</span>
+            <input
+              type="checkbox"
+              checked={expense.used}
+              onChange={() => handleCheckboxChange(index)}
+            />
+            <button onClick={() => handleDelete(index)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 
 export default App;
